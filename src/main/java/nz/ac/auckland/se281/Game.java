82cecede5725327eb485ac8c525845aa73e5ead2
Jeruh1105 <naruh061105@gmail.com>
2024-05-09 14:04:21 +1200

    Made play method to print the starting round message

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -8,16 +8,24 @@ public class Game {
 
   private Difficulty difficulty;
   private Choice choice;
+  private int round;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     this.difficulty = difficulty;
     this.choice = choice;
+    this.round = 1;
 
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    // print the number of round
+    MessageCli.START_ROUND.printMessage(Integer.toString(round));
+
+    // Add 1 to the round for the next round
+    round += 1;
+  }
 
   public void endGame() {}
 
