51c21d5d94a4b373782624cd78ecf14448b0766a
Jeruh1105 <naruh061105@gmail.com>
2024-05-09 13:09:08 +1200

    Implemented newGame method to welcome the player

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,8 +6,15 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private Difficulty difficulty;
+  private Choice choice;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
+    this.difficulty = difficulty;
+    this.choice = choice;
+
     // the first element of options[0]; is the name of the player
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
   public void play() {}
