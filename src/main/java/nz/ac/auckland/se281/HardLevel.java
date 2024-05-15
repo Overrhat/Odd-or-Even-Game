d8ea9d7d5ac3c24339e0281e1f8e5f2d31dd7021
Jeruh1105 <naruh061105@gmail.com>
2024-05-15 21:44:34 +1200

    Created a new class HardLevel which implements Level

---
+++ src/main/java/nz/ac/auckland/se281/HardLevel.java
@@ -0,0 +1,34 @@
+package nz.ac.auckland.se281;
+
+import java.util.ArrayList;
+import nz.ac.auckland.se281.Main.Choice;
+
+public class HardLevel implements Level {
+  private Strategy strategy;
+  private ArrayList<Integer> playerInputList;
+  private Choice choice;
+
+  public HardLevel(ArrayList<Integer> playerInputList, Choice choice) {
+    this.strategy = new RandomStrategy();
+    this.playerInputList = playerInputList;
+    this.choice = choice;
+  }
+
+  @Override
+  public void setStrategy(Strategy strategy) {
+    this.strategy = strategy;
+  }
+
+  @Override
+  public int getNum() {
+    // Find which round this is
+    int numOfRound = this.playerInputList.size();
+
+    // Set to the TopStrategy if it's 4th or higher round
+    if (numOfRound >= 4) {
+      this.setStrategy(new TopStrategy());
+    }
+
+    return this.strategy.selectNum(this.playerInputList, this.choice);
+  }
+}
